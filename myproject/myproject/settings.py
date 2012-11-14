7a81396fda36627f9e4b8fe819b634048eea1e76
Mattias Jämting <mattias@jwd.se>
2012-11-14 12:57:37 +0100

    Removed 'src' directory.

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -71,7 +71,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'src'),
     os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'initializr'),
 )
 
